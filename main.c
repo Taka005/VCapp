eb34f942d55c8aa49e116f05732a41e2f5ad32dd
Taka005 <01taka005@gmail.com>
2024-03-04 16:59:48 +0900

    add

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+
+int numToKey(int keyNum){
+  keys[10] = {
+    VK_0,
+    VK_1,
+    VK_2,
+    VK_3,
+    VK_4,
+    VK_5,
+    VK_6,
+    VK_7,
+    VK_8,
+    VK_9
+  }
+
+  return keys[keyNum];
+}
+
+void speak(char word){
+  char command[500] = "start ./Softalk.exe /W:";
+  strcat(command,word);
+
+  system(command);
+}
+
+void main(void){
+  char words[10];
+
+  words[1] = "こっちへ行こう";
+  words[2] = "あっちに敵がいる";
+  words[3] = "";
+  words[4] = "";
+  words[5] = "";
+  words[6] = "";
+  words[7] = "";
+  words[8] = "";
+  words[9] = "";
+  words[0] = "";
+
+  while(1){
+    for(int i = 0;i <= 9;i++){
+      if((GetAsyncKeyState(VK_SHIFT) < 0)&&(GetAsyncKeyState(numToKey(i)) < 0)){
+        speak(words[i]);
+      }
+    }
+  }
+}
